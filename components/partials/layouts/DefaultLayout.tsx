c910403654721462f2eeede60444b20681054e2f
Jerome Haas <j.haas@cyberplanet.ch>
2021-02-02 14:03:10 +0100

    Refactor: solve merge conflict from rebase

--- components/partials/layouts/DefaultLayout.tsx
+++ components/partials/layouts/DefaultLayout.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import DefaultDesktopNav from '../navigation/DefaultDesktopNav';
+import styles from '../../../styles/DefaultLayout.module.scss';
 
 interface Props {
 }
 
 const DefaultLayout: React.FunctionComponent<Props> = ({ children }) => (
-  <div>{children}</div>
+  <div>
+    <DefaultDesktopNav />
+    <div id={styles.wrapper}>
+      {children}
+    </div>
+  </div>
+
 );
 
 export default DefaultLayout;
