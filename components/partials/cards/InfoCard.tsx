2ee8de8d86e1c71604b4e906b0ac6d37e1049a01
Alexandra <donchenkoalexandra@gmail.com>
2021-02-03 20:17:51 +0100

    Implemented: rendering logs in overview and created infoCard component

--- components/partials/cards/InfoCard.tsx
+++ components/partials/cards/InfoCard.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Styles from '../../../styles/InfoCard.module.scss';
 
 interface Props {
+    number:number,
+    text: string
 }
 
-const InfoCard: React.FunctionComponent<Props> = () => null;
+const InfoCard: React.FunctionComponent<Props> = ({ number, text }) => {
+  console.log(number);
+  return (
+    <div className={Styles.cardWrapper}>
+      <div className={Styles.heading}>
+        {text}
+        {' '}
+      </div>
+      <div className={Styles.number}>
+        {number}
+        {' '}
+      </div>
+    </div>
+  );
+};
 
 export default InfoCard;
