4b23b49883253978dacf43a31a485f9f07835a33
Jerome Haas <j.haas@cyberplanet.ch>
2021-02-03 16:49:42 +0100

    Implement: cards for logs and issues

--- components/partials/cards/IssueCard.tsx
+++ components/partials/cards/IssueCard.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import defaultStyles from '../../../styles/cardWrapper.module.scss';
+import issueStyles from '../../../styles/IssueCard.module.scss';
 
 interface Props {
 }
 
-const IssueCard: React.FunctionComponent<Props> = () => null;
+const IssueCard: React.FunctionComponent<Props> = ({
+  type,
+  createdOn,
+  reportedBy,
+}) => (
+  <div className={defaultStyles.cardWrapper}>
+    <div className={defaultStyles.infoWrapper}>
+      <img className={defaultStyles.image} src="https://avatars.githubusercontent.com/u/12762609?s=400&u=0bef9502b9abf3237338056e4e325344dca451ad&v=4" alt="" />
+      <p className={issueStyles.type}>{type}</p>
+      <p className={issueStyles.createdOn}>{createdOn}</p>
+      <p className={issueStyles.reportedBy}>{reportedBy}</p>
+    </div>
+    <div className={defaultStyles.actionsWrapper}>
+      <img className={defaultStyles.action} src="/media/icons/cardOptions/check.svg" />
+      <img className={defaultStyles.action} src="/media/icons/cardOptions/profile.svg" />
+    </div>
+  </div>
+);
 
 export default IssueCard;
--- pages/dashboard/Issues.tsx
+++ pages/dashboard/Issues.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import DashboardLayout from '../../components/partials/layouts/DashboardLayout';
+import CardWrapper from '../../components/partials/cards/cardWrapper';
+import IssueCard from '../../components/partials/cards/IssueCard';
 
 interface Props { }
 
 const Issues: React.FunctionComponent<Props> = () => (
   <DashboardLayout>
-    <div className="foo">I am your lord Issues</div>
+    <CardWrapper>
+      <IssueCard type="Camera" createdOn="3 hours ago" reportedBy="Leonardo Vittorio" />
+      <IssueCard type="Door" createdOn="2 days ago" reportedBy="Berta Cummelas" />
+      <IssueCard type="Camera" createdOn="25 Jan 2021" reportedBy="Leonardo Vittorio" />
+    </CardWrapper>
   </DashboardLayout>
 );
 
