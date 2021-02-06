d510bf6f2d11148d797bbc75d0c55ef964f064e5
Alexandra <donchenkoalexandra@gmail.com>
2021-02-06 12:20:29 +0100

    dashboard and overview cleaned from unnecessary code

--- components/partials/layouts/DashboardLayout.tsx
+++ components/partials/layouts/DashboardLayout.tsx
@@ -6,11 +6,6 @@ import styles from '../../../styles/DashboardLayout.module.scss';
 import DashboardDesktopNav from '../navigation/DashboardDesktopNav';
 import { showDialog } from '../../../redux/actions/dialogstatus-actions';
 
-import fetchUsers from '../../../redux/actions/user-actions';
-import fetchGroups from '../../../redux/actions/group-actions';
-import fetchDoors from '../../../redux/actions/door-actions';
-import fetchLogs from '../../../redux/actions/log-actions';
-
 import { activateBlur, deactivateBlur } from '../../../redux/actions/dialogblur-actions';
 
 interface Props {
@@ -18,29 +13,17 @@ interface Props {
 }
 
 const DashboardLayout: React.FunctionComponent<Props> = ({ children }) => {
-  const [loaded, setLoaded] = useState(false);
-  const users = useSelector((state) => state.userReducer.users);
-  const groups = useSelector((state) => state.groupReducer.groups);
-  const doors = useSelector((state) => state.doorReducer.doors);
-  const logs = useSelector((state) => state.logsReducer.logs);
   const router = useRouter();
   const dispatch = useDispatch();
-
-  const logged = useSelector((state) => state.loginReducer.logged);
   const dialogblur = useSelector((state) => state.dialogblurReducer);
 
   const activateDialogerBlur = () => { dispatch(activateBlur()); };
   const deactivateDialogerBlur = () => { dispatch(deactivateBlur()); };
-
+  const logged = useSelector((state) => state.loginReducer.logged);
   const href = '/Login';
-  // useEffect(() => {
-  //   if (!logged) { router.push(href); }
-  //   dispatch(fetchDoors());
-  //   dispatch(fetchGroups());
-  //   dispatch(fetchUsers());
-  //   dispatch(fetchLogs());
-  //   setLoaded(true);
-  // }, []);
+  useEffect(() => {
+    if (!logged) { router.push(href); }
+  }, []);
 
   if (logged) {
     const cancelDialog = (event) => {
@@ -51,8 +34,6 @@ const DashboardLayout: React.FunctionComponent<Props> = ({ children }) => {
 
     return (
       <>
-        {/* { users && users.length */}
-        {/* && ( */}
         <div id={styles.pageWrapper}>
           <div id={styles.dashboard}>
             <DashboardDesktopNav />
@@ -62,8 +43,6 @@ const DashboardLayout: React.FunctionComponent<Props> = ({ children }) => {
           </div>
           <div onClick={cancelDialog} id={styles.dialogBlur} className={dialogblur.status === 'active' ? styles.active : ''} />
         </div>
-        {/* ) */}
-        {/* } */}
       </>
     );
   }
